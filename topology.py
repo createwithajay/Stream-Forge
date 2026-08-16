bc292e2ad5b236f6b59749747a6517ffd8d04cfa
codewithajay <ajaybeheraeram@gmail.com>
2026-08-16 18:22:14 +0530

    integrate KafkaSource to consume live truck telemetry

--- topology.py
+++ topology.py
@@ -1,11 +1,16 @@
 import bytewax.operators as op
 from bytewax.dataflow import Dataflow
-from bytewax.testing import TestingSource
-from data_source import mock_telemetry
+from bytewax.connectors.kafka import KafkaSource
 
 # Initialize the core dataflow pipeline
 flow = Dataflow("stream_forge_topology")
 
-# Connect the mock telemetry source to the stream
-stream = op.input("mock_input", flow, TestingSource(mock_telemetry))
-op.inspect("print_output", stream)
+# Day 1: Connect directly to the local Kafka broker Role 1 set up
+stream = op.input(
+    "kafka_in", 
+    flow, 
+    KafkaSource(brokers=["localhost:9092"], topics=["truck_telemetry"])
+)
+
+# Print the raw bytes coming from Kafka
+op.inspect("print_raw", stream)
