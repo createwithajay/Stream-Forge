b1850c874c047dbd05578daa5ff459991ca21e0f
codewithajay <ajaybeheraeram@gmail.com>
2026-08-17 17:47:46 +0530

    add json deserialization mapping for raw kafka bytes

--- topology.py
+++ topology.py
@@ -1,16 +1,24 @@
+import json
 import bytewax.operators as op
 from bytewax.dataflow import Dataflow
 from bytewax.connectors.kafka import KafkaSource
 
-# Initialize the core dataflow pipeline
 flow = Dataflow("stream_forge_topology")
 
-# Day 1: Connect directly to the local Kafka broker Role 1 set up
 stream = op.input(
     "kafka_in", 
     flow, 
     KafkaSource(brokers=["localhost:9092"], topics=["truck_telemetry"])
 )
 
-# Print the raw bytes coming from Kafka
-op.inspect("print_raw", stream)
+# Day 2: Safely decode the Kafka byte payload into a Python dictionary
+def parse_payload(msg):
+    key_bytes, value_bytes = msg
+    if value_bytes is None:
+        return None
+    return json.loads(value_bytes.decode('utf-8'))
+
+# filter_map will process the data and drop any empty/invalid messages
+parsed_stream = op.filter_map("parse_json", stream, parse_payload)
+
+op.inspect("print_parsed", parsed_stream)
