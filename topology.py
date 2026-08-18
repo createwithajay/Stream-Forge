11172be6f5b75b8040521c76534bf8131c4ec28d
codewithajay <ajaybeheraeram@gmail.com>
2026-08-18 19:31:17 +0530

    implement temperature bounds filtering logic

--- topology.py
+++ topology.py
@@ -11,14 +11,19 @@ stream = op.input(
     KafkaSource(brokers=["localhost:9092"], topics=["truck_telemetry"])
 )
 
-# Day 2: Safely decode the Kafka byte payload into a Python dictionary
 def parse_payload(msg):
     key_bytes, value_bytes = msg
     if value_bytes is None:
         return None
     return json.loads(value_bytes.decode('utf-8'))
 
-# filter_map will process the data and drop any empty/invalid messages
 parsed_stream = op.filter_map("parse_json", stream, parse_payload)
 
-op.inspect("print_parsed", parsed_stream)
+# Day 3: Filter out faulty sensor data (keep only realistic temps between -20 and 60 Celsius)
+def filter_extreme_temps(data):
+    return -20.0 <= data["temp"] <= 60.0
+
+clean_stream = op.filter("filter_temps", parsed_stream, filter_extreme_temps)
+
+# Verify the final cleaned data
+op.inspect("print_clean_data", clean_stream)
